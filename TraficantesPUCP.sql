446f5665d88164e204131d42636d530b5870a59a
Ztuart <s.cabrera@pucp.pe>
2017-06-09 13:55:24 -0500

    Add files via upload

--- TraficantesPUCP.sql
+++ TraficantesPUCP.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `traficantespucp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `traficantespucp`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: traficantespucp
+-- Host: 127.0.0.1    Database: traficantespucp
 -- ------------------------------------------------------
--- Server version	5.7.17-log
+-- Server version	5.7.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,9 +20,6 @@
 --
 -- Table structure for table `categoria`
 --
-
-CREATE DATABASE `TraficantesPUCP`;
-USE `TraficantesPUCP`;
 
 DROP TABLE IF EXISTS `categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -50,7 +49,7 @@ DROP TABLE IF EXISTS `comentario`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comentario` (
   `idComentario` int(11) NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
+  `comentario` varchar(45) DEFAULT NULL,
   `idPadre` int(11) NOT NULL,
   `idProducto` int(11) NOT NULL,
   PRIMARY KEY (`idComentario`),
@@ -71,49 +70,28 @@ LOCK TABLES `comentario` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `estadoorden`
+-- Table structure for table `migrations`
 --
 
-DROP TABLE IF EXISTS `estadoorden`;
+DROP TABLE IF EXISTS `migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `estadoorden` (
-  `idEstado` int(11) NOT NULL,
-  `nombreEstado` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idEstado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `estadoorden`
+-- Dumping data for table `migrations`
 --
 
-LOCK TABLES `estadoorden` WRITE;
-/*!40000 ALTER TABLE `estadoorden` DISABLE KEYS */;
-/*!40000 ALTER TABLE `estadoorden` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `lugaresreparto`
---
-
-DROP TABLE IF EXISTS `lugaresreparto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lugaresreparto` (
-  `idLugares` int(11) NOT NULL,
-  `nombreZona` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idLugares`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `lugaresreparto`
---
-
-LOCK TABLES `lugaresreparto` WRITE;
-/*!40000 ALTER TABLE `lugaresreparto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lugaresreparto` ENABLE KEYS */;
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -130,16 +108,14 @@ CREATE TABLE `orden` (
   `IdValoracion` int(11) NOT NULL,
   `idProducto` int(11) NOT NULL,
   `idComprador` int(11) NOT NULL,
-  `idEstado` int(11) NOT NULL,
+  `descripcionZona` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idOrden`),
   KEY `fk_Orden_Valoracion1_idx` (`IdValoracion`),
   KEY `fk_Orden_Producto1_idx` (`idProducto`),
   KEY `fk_Orden_Usuario1_idx` (`idComprador`),
-  KEY `fk_orden_EstadoOrden1_idx` (`idEstado`),
   CONSTRAINT `fk_Orden_Producto1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orden_Usuario1` FOREIGN KEY (`idComprador`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Orden_Valoracion1` FOREIGN KEY (`IdValoracion`) REFERENCES `valoracion` (`idValoracion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orden_EstadoOrden1` FOREIGN KEY (`idEstado`) REFERENCES `mydb`.`estadoorden` (`idEstado`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_Orden_Valoracion1` FOREIGN KEY (`IdValoracion`) REFERENCES `valoracion` (`idValoracion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -150,6 +126,31 @@ CREATE TABLE `orden` (
 LOCK TABLES `orden` WRITE;
 /*!40000 ALTER TABLE `orden` DISABLE KEYS */;
 /*!40000 ALTER TABLE `orden` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+INSERT INTO `password_resets` VALUES ('stuart@pucp.pe','$2y$10$3VFUsRVgSWpdZTJK6imZD.7DtuBVrv/JX2QoWXTUFTvGmtR0UDLlC','2017-06-09 23:06:16');
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -168,14 +169,13 @@ CREATE TABLE `producto` (
   `horaInicio` time DEFAULT NULL,
   `horaFin` time DEFAULT NULL,
   `fechaFin` varchar(45) DEFAULT NULL,
-  `idLugares` int(11) NOT NULL,
+  `zonaReparto` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idProducto`),
   KEY `fk_Producto_Usuario1_idx` (`idVendedor`),
   KEY `fk_Producto_Categoria1_idx` (`idCategoria`),
-  KEY `fk_producto_LugaresReparto1_idx` (`idLugares`),
   CONSTRAINT `fk_Producto_Categoria1` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Producto_Usuario1` FOREIGN KEY (`idVendedor`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_producto_LugaresReparto1` FOREIGN KEY (`idLugares`) REFERENCES `mydb`.`lugaresreparto` (`idLugares`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_Producto_Usuario1` FOREIGN KEY (`idVendedor`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -208,7 +208,38 @@ CREATE TABLE `rol` (
 
 LOCK TABLES `rol` WRITE;
 /*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (0,'usuario'),(1,'administrador');
 /*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'stuart','stuart@pucp.pe','$2y$10$APeF0YPVX28tuVwdHy61Veg5/wDkhtpk.jXot7Wj3gHMRX/3.ySpa','VcdW9iEqLuiaG3PLTxhQfOp0EGya2r6WDxy6JPiT8zcCROZtSj4TAqL2YjOd','2017-06-09 22:56:55','2017-06-09 22:56:55'),(2,'stuart','stuart@gmail.pe','$2y$10$e2J83961zhYUMKbmZo/uJO8/ezViyGokJxDRrQekjYUvWLzeUKFaW','Gkj89jMhOTCMdddXjzbUVTeMFxu2e5hblXmkozfLzrZUzS6WnHPMFmShIswC','2017-06-09 23:04:52','2017-06-09 23:04:52');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -228,6 +259,7 @@ CREATE TABLE `usuario` (
   `estado` int(11) DEFAULT NULL,
   `idrol` int(11) NOT NULL,
   `numReportes` int(11) DEFAULT NULL,
+  `hash` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idUsuario`),
   KEY `fk_Usuario_Rol_idx` (`idrol`),
   CONSTRAINT `fk_Usuario_Rol` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idRol`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -240,6 +272,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'stuart','cab','stuart@pucp.pe','stuart','stu',1,1,0,NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -278,4 +311,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-23 18:35:29
+-- Dump completed on 2017-06-09 13:54:24
